3940c3415114574f75d2d329c9a11a0ad54832bb
swapnil bhakare <swapnilbhakre7@gmail.com>
2021-02-03 17:31:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <main>
       <section className="container">
-        <h3>{people.length} birthday today</h3>
+        <h3>{people.length} Birthday today</h3>
         <List people={people} />
 
         <button onClick={() => setPeople([])}>Clear All</button>
